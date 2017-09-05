df0d82ec9f58abc9b757b4574a35404d62d03eb4
lulisheng <334515218@qq.com>
2017-09-05 18:19:27 +0800

    lulisheng

---
+++ gradleentity/src/main/java/entity/CustomerEntity.java
@@ -0,0 +1,22 @@
+package entity;
+
+public class CustomerEntity {
+    private int id;
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
